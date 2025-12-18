0e6dd824ab0f7ad6862b387216e75a8945960c0c
1tony0 <souza1tony@gmail.com>
2025-12-18 09:32:16 -0500

    feat: Implement dark theme and add Home section with styled components

---
+++ src/styles/Themes.js
@@ -0,0 +1,25 @@
+
+
+export const dark = {
+    body: '#202020',
+    text: '#ffff',
+    bodyRgba: '32,32,32',
+    textRgba: '255,255,255',
+
+    grey: '#bebebe',
+
+    frontxs: '0.75em',
+    frontsm: '0.875em',
+    frontmd: '1em',
+    frontlg: '1.25em',
+    frontxl: '2em',
+    frontxxl: '3em',
+    frontxxxl: '5em',
+    frontBig: '10em',
+
+
+    navHeight: 'Srem'
+
+
+
+}
