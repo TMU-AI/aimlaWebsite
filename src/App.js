4589a8f9b7eb9643df4d6d4905259b66694fdf7b
1tony0 <souza1tony@gmail.com>
2025-12-17 20:20:36 -0500

    feat: Initialize React application with essential files and global styles
    - Added index.js to bootstrap the React application with React Router.
    - Included logo.svg for branding purposes.
    - Created reportWebVitals.js for performance measurement.
    - Set up testing environment with setupTest.js.
    - Implemented GlobalStyles.js for consistent styling across the application.
    - Added Themes.js for potential theme management (currently empty).

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+
+function App() {
+    return(
+        <>
+        <GlobalStyles />
+
+            <main class='App'>
+               App File 
+            </main>
+            
+        </>
+    );
+}
+
+export default App;
